5b56538e28816f97aa80945b4b00b7e08c074497
Jad Mashtoub <jadmashtoub1@gmail.com>
2021-04-10 20:36:38 +1000

    fizzbuzz

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+
+    class Program
+    {
+        static void Main(String[] args) 
+        {
+        int number;
+            
+            for (number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0) 
+            {
+               Console.WriteLine("FizzBuzz");
+            } 
+            else if (number % 3 == 0) 
+            {
+               Console.WriteLine("Fizz");
+            } 
+            else if (number % 5 == 0) 
+            {
+               Console.WriteLine("Buzz");
+            } 
+            //had to look up what to do on these two lines below
+            else {
+               Console.WriteLine(number);
+            }
+         }
+      }
+   }
+}
